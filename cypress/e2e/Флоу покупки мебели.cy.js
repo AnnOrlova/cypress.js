5fedb499d7a3402eae5310651cd822cf8d793044
Anna <anna.orlovaa.97@gmail.com>
2023-03-07 13:12:44 +0300

    cypress-tests/js

--- cypress/e2e/Флоу покупки мебели.cy.js
+++ cypress/e2e/Флоу покупки мебели.cy.js
@@ -1,7 +1,7 @@
 describe('Покупка мебели. Полный флоу', function () {
 
     it('Покупка товаров', function () {
-       cy.visit('https://testqastudio.me/');
+       cy.visit('https://sh3910517.c.had.su/');
        cy.get('.post-11342 > .product-inner > .product-thumbnail > .woocommerce-LoopProduct-link > .attachment-woocommerce_thumbnail').click();
        cy.get('.summary > .cart > .product-button-wrapper > .quantity > .increase').dblclick();
        cy.intercept('POST', '/product/**').as('ajax-product');
@@ -9,6 +9,7 @@ describe('Покупка мебели. Полный флоу', function () {
        cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click();
        cy.wait('@ajax-product');
        cy.wait('@ajax-reload');
+       cy.get('.cart-panel-content > .modal-header > .modal-title').contains('Ваша Корзина (3)').should('be.visible');
        cy.get('#cart-modal > .off-modal-layer').click();
        cy.get('#menu-top > .menu-item-home > a').click();
        cy.get('.header-search > .search-icon > .razzi-svg-icon').click();
@@ -16,10 +17,24 @@ describe('Покупка мебели. Полный флоу', function () {
        cy.get('.search-submit > .razzi-svg-icon').click();
        cy.intercept('POST', '/product/**').as('ajax-product');
        cy.intercept('/?wc-ajax=get_refreshed_fragments').as('ajax-reload');
+       cy.get('.product_title').contains('КЛЛАРИОН Низкий столик').should('be.visible');
+       cy.get('.sku_wrapper').contains('Артикул:YG23I5UTJ1').should('be.visible');
+       cy.get('.posted_in').contains('Категория:Журнальные столы').should('be.visible');
        cy.get('.summary > .cart > .product-button-wrapper > .single_add_to_cart_button').click();
        cy.wait('@ajax-product');
        cy.wait('@ajax-reload');
+       cy.get('.cart-panel-content > .modal-header > .modal-title').contains('Ваша Корзина (4)').should('be.visible');
+       cy.get('.woocommerce-mini-cart__total').contains('47,800.00 ₽').should('be.visible');
        cy.get('.checkout').click();
+       cy.wait(4500);
+       cy.get(':nth-child(1) > td.product-name > .product-item > .product-name').contains('БРОММС Двухместная кровать').should('be.visible');
+       cy.get(':nth-child(1) > td.product-name > .product-item > .product-name > .product-quantity').contains('× 3').should('be.visible');
+       cy.get(':nth-child(1) > .product-total > .woocommerce-Price-amount > bdi').contains('41,700.00 ₽').should('be.visible');
+       cy.get(':nth-child(2) > td.product-name > .product-item > .product-name').contains('КЛЛАРИОН Низкий столик').should('be.visible');
+       cy.get(':nth-child(2) > td.product-name > .product-item > .product-name > .product-quantity').contains('× 1').should('be.visible');
+       cy.get(':nth-child(2) > .product-total > .woocommerce-Price-amount > bdi').contains('6,100.00 ₽').should('be.visible');
+       cy.get('strong > .woocommerce-Price-amount > bdi').contains('47,800.00 ₽').should('be.visible');
+       cy.get('.wc_payment_method').contains('Оплата наличными при доставке заказа.').should('be.visible');
        cy.get('#billing_first_name').type('Иван');
        cy.get('#billing_last_name').type('Иванов');
        cy.get('#billing_address_1').type('Ленина, 1');
